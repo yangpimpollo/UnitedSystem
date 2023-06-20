1de952aae29878caef79db4bd7b2767308c5c973
YANG PIMPOLLO <PIMPOLLOPIM8@outlook.com>
2023-06-20 00:11:26 -0500

    first commit

---
+++ src/main/java/unitedSys_database/DbAccess.java
@@ -0,0 +1,21 @@
+/*   
+
+UNIVERSIDAD NACIONAL DE INGENIERÍA
+INGENIERÍA ESTADÍSTICA
+
+@author yangpimpollo [LIN CHIU CHEN YANG - 20200403F]
+
+clase para crear la conección con la base de datos
+retornando el objeto Connection
+
+*/
+package unitedSys_database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbAccess {
+    
+}
---
+++ src/main/java/unitedSys_main/MainClass.java
@@ -0,0 +1,25 @@
+/*   
+
+UNIVERSIDAD NACIONAL DE INGENIERÍA
+INGENIERÍA ESTADÍSTICA
+
+@author yangpimpollo [LIN CHIU CHEN YANG - 20200403F]
+
+clase para main del proyecto
+
+*/
+
+package unitedSys_main;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import unitedSys_database.DbAccess;
+
+
+public class MainClass {
+        public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
